4b2830b135cdc413280ff83a7df6ce6d39dfa43c
kanizmitu <134768223+kanizmitu@users.noreply.github.com>
2023-06-17 12:43:56 +0600

    Add files via upload

---
+++ iits-js-course-project-summer23-dom/iits-js-course-project-summer23-dom/app.js
@@ -0,0 +1,94 @@
+
+var Backgr = document.getElementById("iits-bg");
+Backgr.classList.remove('bg-danger');
+Backgr.style.Background ="#C1FF33" ;
+var image = document.getElementById("iits-img");
+image.src = './craft.jpg';
+
+var cpoy = document.getElementById("iits-cr-msg");
+cpoy.textContent = 'Copyright title';
+var btn = document.getElementById("iits-btn");
+btn.classList.remove('bg-danger');
+btn.style.background = "#C1FF33";
+
+var text1 = document.getElementById("iits-name");
+text1.textContent = 'Mitu';
+text1.classList.remove('text-danger');
+text1.style.color ='#C1FF33';
+
+var text2 = document.getElementById("iits-prof");
+text2.textContent = 'a student';
+text2.classList.remove('text-danger');
+text2.style.color ='#C1FF33';
+
+var text3 = document.getElementById("iits-edu");
+text3.textContent = '4th semester';
+text3.classList.remove('text-danger');
+text3.style.color ='#C1FF33';
+
+var text4 = document.getElementById("iits-study");
+text4.textContent = 'BCSE';
+text4.classList.remove('text-danger');
+text4.style.color ='#C1FF33';
+
+var text5 = document.getElementById("iits-school");
+text5.textContent = 'IUBAT';
+text5.classList.remove('text-danger');
+text5.style.color ='#C1FF33';
+
+var text6 = document.getElementById("iits-passion");
+text6.textContent = 'programming';
+text6.classList.remove('text-danger');
+text6.style.color ='#C1FF33';
+
+var text7 = document.getElementById("iits-numYears");
+text7.textContent = '2 years';
+text7.classList.remove('text-danger');
+text7.style.color ='#C1FF33';
+
+var text8 = document.getElementById("iits-hobby");
+text8.textContent = 'reading and problem solving';
+text8.classList.remove('text-danger');
+text8.style.color ='#C1FF33';
+
+var text9 = document.getElementById("iits-crc");
+text9.textContent = 'a passionate about coding';
+text9.classList.remove('text-danger');
+text9.style.color ='#C1FF33';
+
+var text10 = document.getElementById("iits-ability");
+text10.textContent = 'enrich my problem solving ability';
+text10.classList.remove('text-danger');
+text10.style.color ='#C1FF33';
+
+var text11 = document.getElementById("iits-goal");
+text11.textContent = 'my problem solving skill';
+text11.classList.remove('text-danger');
+text11.style.color ='#C1FF33';
+
+var fb = document.getElementById('iits-fb-link');
+fb.href = 'https://www.facebook.com/profile.php?id=100090520878494';
+
+var mail = document.getElementById('iits-email');
+mail.href = "mailto:22103111@iubat.edu";
+
+var github = document.getElementById('iits-github-link');
+github.href = "https://github.com/kanizmitu/assignment.git";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
